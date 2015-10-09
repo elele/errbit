7fce005ac50eb551cb442c6d18fce76897556c4e
elele <elele@eleledeMacBook-Pro.local>
2015-10-09 10:51:14 +0800

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -2,4 +2,6 @@ daemonize true
 environment 'production'
 workers 0
 bind 'tcp://0.0.0.0:3000'
+state_path '/srv/errbit/shared/tmp/sockets/puma.state'
+activate_control_app 'unix:///srv/errbit/shared/tmp/sockets/pumactl.sock'
 preload_app!
